d9c2e68c272d9e0588487ec86a372ac20f43f998
satendra-patel <patelsatendra06@gmail.com>
2023-04-03 22:47:45 +0530

    signuppage

---
+++ Frontend/signup.js
@@ -0,0 +1,44 @@
+const myForm = document.getElementById('signupform');
+
+myForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    try {
+        const res = await axios.post('', 
+        {
+            name: name.value, 
+            email: email.value, 
+            password: password.value
+        }
+        );
+        console.log('SIGN UP RESPONSE: ', res);
+        if(res.status === 200){
+            name.value = '';
+            email.value = '';
+            password.value = '';
+            clearError();
+            
+        }
+        
+    } catch (error) {
+        logErrorToUser(error);
+        if(error.response.status === 400) {
+            alert('User already exists!');
+        }
+        console.log(error);
+    }
+});
+
+function logErrorToUser(error) {
+    const err = document.getElementById('error-text');
+    err.innerHTML = error.message;
+};
+
+function clearError() {
+    const err = document.getElementById('error-text');
+    err.innerHTML = '';
+};
